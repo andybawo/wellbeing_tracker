7b0c11274819fcf0768d6c2236d336ab30451a12
andybawo <andybawo777@gmail.com>
2025-04-01 16:57:21 -0700

    sidebar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, NgModule } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { ButtonComponent } from '../shared/components/button/button.component';
 import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, CommonModule, ButtonComponent, SharedModule],
+  imports: [RouterOutlet, CommonModule, SharedModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })

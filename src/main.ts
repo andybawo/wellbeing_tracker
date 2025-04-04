c3a989bc0f029ab4d7ced36e50d201e81ab88922
andybawo <andybawo777@gmail.com>
2025-04-03 18:16:16 -0700

    core module setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { CoreModule } from './app/core/core.module';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [...CoreModule.providers],
+}).catch((err) => console.error(err));

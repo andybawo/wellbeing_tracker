66f86880f95acc4d71b6db93e3639eaef1fe9a3a
andybawo <andybawo777@gmail.com>
2025-06-20 14:50:15 -0700

    adjustments

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -19,6 +19,11 @@ import { Component, Input } from '@angular/core';
         align-items: center;
         justify-content: center;
         margin: auto;
+        margin-top: auto;
+        position: fixed; /* Make it a fixed overlay */
+        top: 0; /* Position it at the top of the viewport */
+        left: 0; /* Position it at the left of the viewport */
+        z-index: 1001; /* Ensure it's above other content (adjust as needed) */
       }
 
       .error {

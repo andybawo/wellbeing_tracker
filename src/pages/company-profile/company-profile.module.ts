c2e79010037ff1f8815ab89dc7ddf4952c5b8004
jalal-codr <abduljalal849@gmail.com>
2025-07-22 13:52:06 +0100

    Added profile screen to company profile dashboard.

--- src/pages/company-profile/company-profile.module.ts
+++ src/pages/company-profile/company-profile.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileLayoutComponent } from './components/profile-layout/profile-layout.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { CompanyProfileRoutingModule } from './company-profile-routing.module';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { CompanyProfileRoutingModule } from './company-profile-routing.module';
   imports: [
     CommonModule,
     CompanyProfileRoutingModule,
-    ProfileLayoutComponent // Importing the standalone component
+    ProfileLayoutComponent, 
+    ProfileComponent 
   ]
 })
 export class CompanyProfileModule { }
--- src/pages/company-profile/components/profile-layout/profile-layout.component.ts
+++ src/pages/company-profile/components/profile-layout/profile-layout.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-profile-layout',
+  standalone: true,
+  imports: [ProfileComponent],
   templateUrl: './profile-layout.component.html',
   styleUrls: ['./profile-layout.component.scss']
 })
